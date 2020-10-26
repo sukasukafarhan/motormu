f43d390213399c061fe92261bd13c2bb52be0e8b
sukasukafarhan <farhanmfap2604@gmail.com>
2020-10-26 16:20:11 +0700

    Added my project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 // $(document).ready(function() {
 //     $('.caricepatkategori').select2();
 // });
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:40,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+  })
+});
