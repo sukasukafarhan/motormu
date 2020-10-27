7fe450c2542f525f605ea31f219735b0e1f8e517
sukasukafarhan <farhanmfap2604@gmail.com>
2020-10-27 18:27:48 +0700

    Added my project

--- js/script.js
+++ js/script.js
@@ -19,3 +19,35 @@ $(document).ready(function(){
     }
   })
 });
+
+let thumbnails = document.getElementsByClassName('thumbnail')
+
+let activeImages = document.getElementsByClassName('active')
+    for (var i=0; i < thumbnails.length; i++){
+        thumbnails[i].addEventListener('click', function(){
+            console.log(activeImages)            
+                if (activeImages.length > 0){
+                    activeImages[0].classList.remove('active')
+                }
+
+                this.classList.add('active')
+                document.getElementById('featured').src = this.src
+        })
+    }
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+buttonLeft.addEventListener('click', function(){
+    document.getElementById('slider').scrollLeft -= 180
+})
+buttonRight.addEventListener('click', function(){
+    document.getElementById('slider').scrollLeft += 180
+})
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
